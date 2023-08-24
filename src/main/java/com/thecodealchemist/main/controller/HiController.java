a72f9ceafbb95a5bc0749efaa7a680050b410aec
therealdumbprogrammer <therealdumbprogrammer@gmail.com>
2023-08-24 12:46:46 +0530

    Adding Spring Cloud Netflix Eureka Client

---
+++ src/main/java/com/thecodealchemist/main/controller/HiController.java
@@ -0,0 +1,25 @@
+package com.thecodealchemist.main.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping
+    public String hi() {
+        discoveryClient.getInstances("myEurekaClient").forEach(serviceInstance -> {
+            System.out.println("============ START ===============");
+            System.out.println(serviceInstance.getUri());
+            System.out.println(serviceInstance.getPort());
+            System.out.println(serviceInstance.getServiceId());
+            System.out.println(serviceInstance.getHost());
+        });
+        return "Hi";
+    }
+}
